079c3ba652a413e3c8b209a4633e710d02020332
yuji91 <yuji91.1227@gmail.com>
2019-05-01 20:14:50 +0900

    Add default locale setting

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Exam01:Bugfix
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
