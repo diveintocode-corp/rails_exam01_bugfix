280eaa85ed4c1b29e5cfef61576f040b290f2b2b
yuji91 <yuji91.1227@gmail.com>
2019-05-01 20:12:22 +0900

    Add timezone to config

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Exam01:Bugfix
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
