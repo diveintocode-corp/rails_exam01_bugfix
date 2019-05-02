f9e9e31aad209852a4396401e25a1db674cdacc1
yuji91 <yuji91.1227@gmail.com>
2019-05-02 19:04:18 +0900

    Add controller comments [create, destroy]

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  before_action :set_blog, only: [:create, :destroy]
+
+  def create
+    @comment = @blog.comments.create(comment_params)
+    redirect_to blog_path(@blog)
+  end
+
+  def destroy
+    @comment = @blog.comments.find(params[:id])
+    @comment.destroy
+    redirect_to blog_path(@blog)
+  end
+
+  private
+    def set_blog
+      @blog = Blog.find(params[:blog_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:name, :content, :blog_id)
+    end
+end
