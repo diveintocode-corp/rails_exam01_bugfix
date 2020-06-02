9037bbc76e173675d33c6198cf771b9f5eee5190
yuki_tamura <yuki_tamura@diveintocode.jp>
2020-06-02 11:45:54 +0900

    Modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do
   title = Faker::Lorem.word
-  content = Faker::Lorem.sentences(rand(1..3)).join("\n")
+  content = Faker::Lorem.sentences(number: rand(1..3)).join("\n")
   Blog.create(
     title: title,
     content: content
